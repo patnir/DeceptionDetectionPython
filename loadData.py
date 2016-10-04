fa01de86fd90ab75dd558142defbaa885d4b1c2c
patnir <rahul20patni@gmail.com>
2016-10-04 00:51:27 -0400

    data loading

--- loadData.py
+++ loadData.py
@@ -9,12 +9,20 @@ Created on Wed Sep 28 11:29:54 2016
 
 
 def loadData():
-    lies = "100OnlyLies.csv"
-    truth = "100OnlyTruths.csv"
-    lptr = open(lies)
-    for i in lptr:
-        i = i.rstrip()
-        print i
+    files = ["100LiesAndLabels.csv", "100TruthsAndLabels.csv"]
+    words = set()
+    for x in files:
+        lines = open(x)
+        for i in lines:
+            i = i.rstrip()
+            i = i.split('.\',')
+            j = i[0].split(' ')
+            for k in j:
+                words.add(k)
+    words = list(words)
+    words.sort()
+    print words
+        
     
     
 def main():
